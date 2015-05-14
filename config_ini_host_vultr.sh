9973534a8af97113091bd9119f64f2582eb7d384
Kike <kike@vultr.guest>
2015-05-14 09:44:57 +0000

    Se elimina la instalacion del paquete auto-complete, ya que no existe
    Signed-off-by: Kike <kike@vultr.guest>

--- config_ini_host_vultr.sh
+++ config_ini_host_vultr.sh
@@ -6,7 +6,7 @@ apt-get update && apt-get upgrade
 cd $HOME
 
 echo "Instalando paquetes necesarios"
-apt-get install git git-core gawk auto-complete bash-completion rxvt-unicode-256color vim-nox pkg-config
+apt-get install git git-core gawk bash-completion rxvt-unicode-256color vim-nox pkg-config
 
 echo "Clonando Mi espacio de trabajo...."
 git clone https://github.com/emoronayuso/My-workspace.git && cd My-workspace/ && cp .bash_aliases .. && cp .bashrc .. && cp .vimrc .. && cp .bash_profile ..
